9324d96cf1cab30ff3b6cd291819116dfcc800ec
Carter <hamming1@gmail.com>
2016-03-31 21:32:01 +0800

    add color log

---
+++ README.md
@@ -0,0 +1,8 @@
+### Reference
+1. cmd with go
+* https://godoc.org/gopkg.in/alecthomas/kingpin.v2#ArgClause.URL
+* https://developer.atlassian.com/blog/2016/03/building-helpful-golang-cli-tools/
+* https://github.com/alecthomas/kingpin
+
+2. go scraper
+http://schier.co/blog/2015/04/26/a-simple-web-scraper-in-go.html
--- main.go
+++ main.go
@@ -1,12 +1,78 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"net/http"
+	"net/url"
+
+	"github.com/op/go-logging"
+	"golang.org/x/net/html"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+var (
+	distURL = kingpin.Arg("url", "url to parse").Required().String()
+)
+
+var log = logging.MustGetLogger("example")
+
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
 func main() {
-	app := kingpin.New("my-app", "My Example CLI Application With Bash Completion")
-	kingpin.MustParse(app.Parse(os.Args[1:]))
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+	fmt.Printf("----- Parse URL : %s\n", *distURL)
+
+	// For demo purposes, create two backend for os.Stderr.
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+
+	// For messages written to backend2 we want to add some additional
+	// information to the output, including the used log level and the name of
+	// the function.
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+
+	// Only errors and more severe messages should be sent to backend1
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+
+	// Set the backends to be used.
+	logging.SetBackend(backend1Leveled, backend2Formatter)
+
+	_, err := url.Parse(*distURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, _ := http.Get(*distURL)
+	//bytes, _ := ioutil.ReadAll(resp.Body)
+
+	//fmt.Println("HTML:\n\n", string(bytes))
+	z := html.NewTokenizer(resp.Body)
+	for {
+		tt := z.Next()
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			isAnchor := t.Data == "a"
+			if isAnchor {
+				fmt.Printf("We found a link!\n")
+				for _, a := range t.Attr {
+					if a.Key == "href" {
+						log.Info("Found href:", a.Val)
+						break
+					}
+				}
+			}
+		}
+	}
+	resp.Body.Close()
 }
